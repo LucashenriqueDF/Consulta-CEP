18d67acdd2cd7b16b40bfeacd8f0934316c8697a
LucasHDLLF <gameslucas999@gmail.com>
2022-09-15 18:00:26 -0400

    1.1(Visualização dos dados para o usuário )

---
+++ public/app.js
@@ -0,0 +1,47 @@
+//Consulta de CEP na API
+  const cep = document.getElementById("inputEntradaDeDados")
+  const button = document.getElementById("btn")
+
+  console.log(cep)
+  async function get_cep (cep) {
+  if(cep.length !== 8) {
+  alert("cep inválido")
+  }
+  const url = `http://viacep.com.br/ws/${cep}/json/`
+  const response = await fetch (url)
+  
+  return response.json().this(MostrarResultado)
+  
+  }
+
+  button.addEventListener("click",async () => {
+    const cep_value = cep.value
+    const data = await get_cep(cep_value)
+
+    console.log(data);
+    })
+  
+    
+    
+    
+    function MostrarResultado(dados) {
+    const result = document.getElementById("resultado");
+    if(dados.erro){
+      result.innerHTMl("Não foi possivel localizar endereço");
+    }else{
+      result.innerHTMl=    `<p>logradouro:${dados.logradouro}</p> 
+                            <p>bairro:${dados.bairro} </p>
+                            <p>uf:${dados.uf} </p>
+                            <p>ibge:${dados.ibge} </p>
+                            <p>ddd:${dados.ddd} </p> ` 
+                    
+
+    }
+
+    
+  }
+
+
+
+
+
